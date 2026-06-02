a89d235ef222f16cbc8901dd45bf79012deecf30
Arcitta Kinanthi <cittacittul@gmail.com>
2026-06-02 16:52:14 +0700

    Menambahkan daftar screen navigasi OtoCare

---
+++ app/src/main/java/com/arcittakinanthi/otocare/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.arcittakinanthi.otocare.navigation
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object FormBaru : Screen("detail/0")
+    data object FormUbah : Screen("detail/{id}") {
+        fun withId(id: Long) = "detail/$id"
+    }
+}
